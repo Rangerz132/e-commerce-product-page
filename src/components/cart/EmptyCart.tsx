d40683375a311a20f8c75255df9c20299e0d5f5c
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2025-02-16 14:05:00 -0500

    add read me

--- src/components/cart/EmptyCart.tsx
+++ src/components/cart/EmptyCart.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const EmptyCart = () => {
   return (
     <div className="flex items-center justify-center py-20 text-neutral-dark-grayish-blue font-bold w-full">
--- src/pages/Men.tsx
+++ src/pages/Men.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Men = () => {
-  return (
-    <div>Men</div>
-  )
-}
+  return <div>Men</div>;
+};
 
-export default Men
+export default Men;
