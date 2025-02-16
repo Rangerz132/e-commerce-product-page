27b4734657f46a36bb05edd883c8f54b7cd7540b
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2025-02-16 13:46:27 -0500

    fix cart

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -3,6 +3,7 @@ import { CartContext, useCartContext } from "../../contexts/CartContext";
 import CartItem from "./CartItem";
 import EmptyCart from "./EmptyCart";
 import { RootState } from "../../state/store";
+import Button from "../button/Button";
 
 const Cart = () => {
   const { cart } = useCartContext(CartContext);
@@ -16,16 +17,30 @@ const Cart = () => {
         }`}
       >
         <div className="bg-white rounded-lg shadow-xl w-full">
+          {/** Title */}
           <div className="font-bold p-6 border-b-[0.5px] border-b-neutral-200">
             Cart
           </div>
-          <div className="p-6 w-full">
+          <div className="p-6 w-full flex flex-col space-y-3">
+            {/** Empty cart */}
             {cartProductList.length == 0 ? (
               <EmptyCart />
             ) : (
-              cartProductList.map((item, index) => (
-                <CartItem key={index} cartProductItem={item} />
-              ))
+              <>
+                {/** Cart product items */}
+                {cartProductList.map((item, index) => (
+                  <CartItem key={index} cartProductItem={item} />
+                ))}
+                {/** Checkout button */}
+                <Button
+                  onClick={() => {
+                    console.log("checkout...");
+                  }}
+                  className="button-1 w-full py-4"
+                >
+                  Checkout
+                </Button>
+              </>
             )}
           </div>
         </div>
